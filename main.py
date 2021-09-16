8cd5e45beca267ed0be30cc1eb26b05953abee35
AndrewGrabowski2024 <819788@mystma.org>
2021-09-16 15:18:58 -0500

    changes

--- main.py
+++ main.py
@@ -1,8 +1,72 @@
-#printy first movie
-print("Your name")
+#define function
+def topFiveMovies():
+    #print first movie
+    print("your Name")
 
-#print second movie
-print("annabelle")
+    #print second movie
+    print("lord of the rings")    
+    
+    #print third movie
+    print("Shang-chi")   
+    
+    #print fourth movie
+    print("the hobbit")
 
-#print third movie 
-print("shang-chi and the ten rings")
+    #print fifth movie
+    print("anything")
+
+topFiveMovies()
+#define function 2
+def topThreeVideoGames():
+        #print first videogame
+        print("zelda ocarina of time")
+
+        #print second video game
+        print("zelda twilight princess")
+
+        #print third movie
+        print("mario cart wii")
+
+topThreeVideoGames()
+print("cat" * 420)
+
+def myNameIs(MEWHENYOURMOM):
+    #myname
+    print(MEWHENYOURMOM + " is my name")
+
+#TaE
+myNameIs("Andrew") 
+myNameIs("turtle")
+
+#define fuction 3
+def timesTen(num):
+    print(num*10)
+
+timesTen(42)
+
+#define fuction 4
+def plusFive(num):
+    print(num + 5)
+
+#define fuction 5
+def dividedByTwo(num):
+    print(num / 2)
+    
+#TaE
+plusFive(64)
+dividedByTwo(10)
+
+#fuction number 6
+def addTwoNumbers(x,y):
+    print(x,y)
+#TaE
+addTwoNumbers(3248,19384)
+
+
+
+def madLib(asd, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r):
+    print("while i stayed at home, ivbe practiced the"+ asd + "for" + b + "hours every"+ c + ". My favirite key signiture to play and practice in is" + d + e + ". it has" + f + "flats and" + g +"sharps. I like this key signiture because it is" + h + "and" + j +". I've also gotten awesome at counting timne signatures. The time signature i love is" + k + ", because it is" + l + "to count. One instrument i want to learn how to play while im at home is" + m + "because it is" + n + "and sounds like a" + o + ". My" + p + "likes it when i play" + q + ", and akways gives me a round of applause after my performances! When i return to school, my teacher will be" + r + "of how great i am playing my instrument. My teacher might even think i sound like" + s + "!")
+
+
+madLib("mayonaise", "10", "sunday","E","major","3723043579","1","submissive","breedable","16532/3","looooong","horse radish", "chonky","jimmys mom","dad","astronaut in the ocean","pumped","dababy")
+
